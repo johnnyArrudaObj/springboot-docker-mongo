472620ec731505294d48ca01d542ba70dd55058d
Johnny Arruda <johnny.arruda@objective.com.br>
2023-03-21 13:53:38 -0300

    change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
-# Imagem base com o Java 11
-FROM openjdk:11-jdk-slim
+# Imagem ubuntu
+FROM ubuntu:latest
+
+RUN apt-get update && \
+    apt-get install -y openjdk-17-jdk
+
+ENV JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
+ENV PATH $JAVA_HOME/bin:$PATH
 
 # Definir o diretório de trabalho para o aplicativo
 WORKDIR /app
--- src/main/java/com/johnny/customerapi/entity/customer/CustomerDTO.java
+++ src/main/java/com/johnny/customerapi/entity/customer/CustomerDTO.java
@@ -4,7 +4,6 @@ import com.johnny.customerapi.entity.specialty.Specialty;
 import com.johnny.customerapi.entity.address.AddressDTO;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
